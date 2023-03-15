5e6dda1af623e1b623e788acbdc4f1c010b0ff67
msin0017 <msin0017@student.monash.edu>
2023-03-16 08:41:00 +1100

    For the time being, I am going with bubble, selection and insertion sort on the console. I will try to integrate this project with a graphics library, to make  GUI later, if I have time over the weekend :|

--- main.cpp
+++ main.cpp
@@ -4,7 +4,21 @@
 #include <chrono>
 #include <functional>
 
+//driver of the sorting functions
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    /*initializing a standard array, for example, to emulate data which I want to visualize being sorted.
+    It really doesn't matter what the array is, it can be any series of positive integers.*/
+    std::vector<int> arr = {8, 7, 6, 5, 4, 3, 2, 1};
+    //variable to hold the choice input from the user
+    int choice;
+    //printing out the menu!! :)
+    std::cout << "Select sorting algorithm:" << endl
+              << "1. Bubble Sort" << endl
+              << "2. Selection Sort" << endl
+              << "3. Insertion Sort" << endl
+              << "Enter your choice (1-3): ";
+    //taking the input from the user
+    std::cin >> choice;
+
     return 0;
 }
